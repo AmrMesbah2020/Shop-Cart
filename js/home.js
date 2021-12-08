d120382979e116600fa150c631aee835879931c3
AlaaAlSayed <alaa.elfaioumy@gmail.com>
2021-12-08 12:11:25 +0200

    activating header's select menu
    1-Directing select menu into pages
    ( Updated:  home.js & nav bar in home.html only )
    2-Adding categories tab into nav bar

--- js/home.js
+++ js/home.js
@@ -15,5 +15,40 @@ function display(){
 var interval=setInterval(display,3000);
 
 
+// directing select menu options to selected category : 
+
+
+var btnLiv = document.getElementsByClassName("btnLiv");
+var btnKit = document.getElementsByClassName("btnKit");
+var btnBed = document.getElementsByClassName("btnBed");
+var btnDin = document.getElementsByClassName("btnDin");
+
+for (let i = 0; i < btnLiv.length; i++) {
+    btnLiv[i].addEventListener('click', function () {
+        location.href = "./productsLiv.html";
+    }); 
+}
+
+for (let i = 0; i < btnKit.length; i++) {
+    btnKit[i].addEventListener('click', function () {
+        location.href = "./productsKit.html";
+    }); 
+}
+
+for (let i = 0; i < btnBed.length; i++) {
+    btnBed[i].addEventListener('click', function () {
+        location.href = "./productsBed.html";
+    }); 
+}
+
+
+for (let i = 0; i < btnDin.length; i++) {
+    btnDin[i].addEventListener('click', function () {
+        location.href = "./productsDin.html";
+    }); 
+}
+
+
+
 
 
